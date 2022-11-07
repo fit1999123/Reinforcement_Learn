d7ab41c8577b5931568e1de4a675eeb366c1ade4
fit1999123 <33652029+fit1999123@users.noreply.github.com>
2022-11-07 23:33:30 +0800

    Update CliffWalking_with_QLeaning_and_SARSA.py

--- CliffWalking_with_QLeaning_and_SARSA.py
+++ CliffWalking_with_QLeaning_and_SARSA.py
@@ -5,20 +5,19 @@ import matplotlib.pyplot as plt
 
 
 env = gym.make("CliffWalking-v0")
-env = env.unwrapped
+
 action_space_n = env.action_space.n
 
 state_n = env.observation_space.n
 
-episodes = 20000
+episodes = 2000
 
-alpha = 0.8
+alpha = 0.1
 
 n_step = 50
 
-gamma = 0.7
+gamma = 0.75
 
-epsilon = 1.0
 
 def arg_max(lst):
     return lst.index(max(lst))
@@ -34,7 +33,7 @@ def greedy(Q_table,state,epsilon):
 
     else:
                     
-        action = int(env.action_space.sample())
+        action = env.action_space.sample()
 
     return action
     
@@ -47,7 +46,7 @@ class robot():
     def __init__(self):
         
 
-        self.state = env.reset()
+        self.state = env.reset()[0]
 
         self.lst = []
 
@@ -71,13 +70,12 @@ class robot():
                     
                 self.Q_table[self.state][action] += alpha*(reward-self.Q_table[self.state][action])
                         
-                self.state = env.reset()
-                    
-                break
+                self.state,_ = env.reset()
+ 
                     
             else:
                         
-                self.Q_table[self.state][action] += alpha*(reward+gamma*(self.Q_table[new_state][next_action]-self.Q_table[self.state][action]))
+                self.Q_table[self.state][action] += alpha*(reward+gamma*self.Q_table[new_state][next_action]-self.Q_table[self.state][action])
                         
                 self.state = new_state
                     
@@ -97,111 +95,101 @@ class robot():
                     
                 self.Q_table[self.state][action] += alpha*(reward-self.Q_table[self.state][action])
                         
-                self.state = env.reset()
-                    
-                break
-                    
+                self.state,_ = env.reset()
+
             else:
                         
-                self.Q_table[self.state][action] += alpha*(reward+gamma*(self.Q_table[new_state][next_action]-self.Q_table[self.state][action]))
+                self.Q_table[self.state][action] += alpha*(reward+gamma*self.Q_table[new_state][next_action]-self.Q_table[self.state][action])
                         
                 self.state = new_state
 
     def Expected_SARSA(self,epsilon):
 
-        for i in range(n_step):
-                    
-         
-            action = greedy(self.Q_table,self.state,epsilon)
+            for i in range(n_step):
+            
+                action = greedy(self.Q_table,self.state,epsilon)
 
+                new_state,reward,terminated,truncated,info =  env.step(action)
 
-            new_state,reward,terminated,truncated,info =  env.step(action)
-
-            expected_value = sum(self.Q_table[new_state])/len(self.Q_table[new_state])
-
-            if terminated or truncated:
-                    
-                self.Q_table[self.state][action] += alpha*(reward-self.Q_table[self.state][action])
+                if terminated or truncated:
                         
-                self.state = env.reset()
-                    
-                break
-                    
-            else:
+                    self.Q_table[self.state][action] += alpha*(reward-self.Q_table[self.state][action])
+                            
+                    self.state,_ = env.reset()
                         
-                self.Q_table[self.state][action] += alpha*(reward+gamma*expected_value-self.Q_table[self.state][action])
-                        
-                self.state = new_state
+                else:
+
+                    expected_value = sum(self.Q_table[new_state])/len(self.Q_table[new_state])
+
+                    self.Q_table[self.state][action] += alpha*(reward+gamma*expected_value-self.Q_table[self.state][action])
+                            
+                    self.state = new_state
 
 
     def test(self,algorithm):
 
-        global epsilon
-
+     
+        
         for i in range(episodes):
             
-            if i >= 0.25*episodes and i< 0.5*episodes:
-                
-                epsilon = 0.5
-            
-            elif i>=0.5*episodes:
-                
+            if i>10:
+
                 epsilon = 0.1
-            
+            else:
+               epsilon = 1.0
+
             algorithm(epsilon)
 
-            self.lst.append(sum(self.Q_table[7]))
+            self.lst.append(sum(self.Q_table[0]))
 
 my_robot = robot()
 my_robot2 = robot()
-my_robot3 = robot()
+
 my_robot.test(my_robot.Q_learning)
 my_robot2.test(my_robot2.SARSA)
-my_robot3.test(my_robot3.Expected_SARSA)
-env = gym.make("CliffWalking-v0",render_mode="human")
-my_robot.state = env.reset()
 
-while True:
+env.close()
+
+env = gym.make("CliffWalking-v0",render_mode="human")
+my_robot.state,_ = env.reset()
+
+for i in range(30):
 
     action = arg_max(my_robot.Q_table[my_robot.state])
-    lst2 = env.step(action)
-    my_robot.state = lst2[0]
-    if lst2[2] == True or lst2[3] == True:
+    result = env.step(action)
+    my_robot.state = result[0]
+    if result[2] == True or result[3] == True:
                 
         break
-my_robot2.state = env.reset()
-while True:
+
+env.close()
+
+env2= gym.make("CliffWalking-v0",render_mode="human")
+my_robot2.state,_ = env2.reset()
+for i in range(30):
 
     action = arg_max(my_robot2.Q_table[my_robot2.state])
-    result = env.step(action)
+    result = env2.step(action)
     my_robot2.state = result[0]
     if result[2] == True or result[3] == True:
                 
         break
-my_robot3.state = env.reset()
-while True:
+env2.close()
 
-    action = arg_max(my_robot3.Q_table[my_robot3.state])
-    result = env.step(action)
-    my_robot3.state = result[0]
-    if result[2] == True or result[3] == True:
-                
-        break
+
 
 
 
 
 lst = my_robot.lst
 lst2 = my_robot2.lst
-lst3 = my_robot3.lst
 x = [i for i in range(episodes)]
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 ax.plot(x,lst)
 ax.plot(x,lst2)
-ax.plot(x,lst3)
+plt.legend(['Q-learning','Sarsa'])
 plt.show()
 plt.close()
-
 
 
